5d01fe8f16cce80082276cc0087977c0082004b4
Victor Vermaas <vhvermaas@gmail.com>
2022-08-03 21:48:35 -0300

    round-progressbar

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -1,4 +1,5 @@
-
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import styled from "styled-components"
 
 export default function BottomMenu() {
@@ -6,7 +7,42 @@ export default function BottomMenu() {
         <Wrapper>
             <span>Hábitos</span>
 
-            <div></div>
+            <div><CircularProgressbar value={60} text={`Hoje`} styles={{
+    // Customize the root svg element
+    root: {},
+    // Customize the path, i.e. the "completed progress"
+    path: {
+      // Path color
+      stroke: `white`,
+      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+      strokeLinecap: 'round',
+      // Customize transition animation
+      transition: 'stroke-dashoffset 0.5s ease 0s',
+      // Rotate the path
+      transformOrigin: 'center center',
+    },
+    // Customize the circle behind the path, i.e. the "total progress"
+    trail: {
+      // Trail color
+      stroke: '#52B6FF',
+      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+      strokeLinecap: 'butt',
+      // Rotate the trail
+      transform: 'rotate(0.25turn)',
+      transformOrigin: 'center center',
+    },
+    // Customize the text
+    text: {
+      // Text color
+      fill: '#ffffff',
+      // Text size
+      fontSize: '20px',
+    },
+    // Customize background - only used when the `background` prop is true
+    background: {
+      fill: '#3e98c7',
+    },
+  }}/></div>
 
             <span>Histórico</span>
         </Wrapper>
@@ -35,6 +71,7 @@ span {
 div {
     height: 90px;
     width: 90px;
+    border: #52B6FF solid 4px;
     border-radius: 70px;
     background: #52B6FF;
     margin-bottom: 40px;
